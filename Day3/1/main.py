9b793220be8f894c4cf2b5e3c96fa51b30ada19c
Alejandro Uribe <jauriberamirez@gmail.com>
2022-01-18 01:07:19 -0500

    Progress in Day 3

--- Day3/1/main.py
+++ Day3/1/main.py
@@ -2,15 +2,15 @@ def run():
     with open('../data/puzzle_input.txt', 'r') as file:
         lines = [str(i.strip()) for i in file.readlines()]
 
-    print(lines[0:][0])
-    top_bits = len(lines)
-    n_bits = len(lines[0])
-    seq_dict = {}
+    seq_dict = {i:'' for i in range(0,len(lines[0]))}
 
-    for i in range(0, n_bits):
-        seq_dict[i] = lines[:][i]
+    for line in lines:
+        for i, char in enumerate(line):
+            seq_dict[i] += char
 
     return seq_dict
+
+
 def int2bin(n):
     return int2bin(n >> 1) + [n & 1] if n > 1 else [1]
 
